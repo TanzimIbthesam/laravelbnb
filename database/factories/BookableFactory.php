f854ffa03b865730f779ef2797489a8831722028
TanzimIbthesam <35595427+TanzimIbthesam@users.noreply.github.com>
2020-10-22 18:16:31 +0600

    Update BookableFactory.php

--- database/factories/BookableFactory.php
+++ database/factories/BookableFactory.php
@@ -1,26 +1,41 @@
 <?php
 
-/** @var \Illuminate\Database\Eloquent\Factory $factory */
+namespace Database\Factories;
 
-use App\Bookable;
-use Faker\Generator as Faker;
+use App\Models\Bookable;
+use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Arr;
 
-$suffix = [
-    'Villa',
-    'House',
-    'Cottage',
-    'Luxury Villas',
-    'Cheap House',
-    'Rooms',
-    'Cheap Rooms',
-    'Luxury Rooms',
-    'Fancy Rooms'
-];
+class BookableFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Bookable::class;
 
-$factory->define(Bookable::class, function (Faker $faker) use ($suffix) {
-    return [
-        'title' => $faker->city . ' ' . Arr::random($suffix),
-        'description' => $faker->text()
-    ];
-});
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        $suffix = [
+            'Villa',
+            'House',
+            'Cottage',
+            'Luxury Villas',
+            'Cheap House',
+            'Rooms',
+            'Cheap Rooms',
+            'Luxury Rooms',
+            'Fancy Rooms'
+        ];
+        return [
+            'title' => $this->faker->city . ' ' . Arr::random($suffix),
+            'description' => $this->faker->text()
+        ];
+    }
+}
